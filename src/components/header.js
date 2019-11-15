c3ce2e9743481b548222baa0723d4ecf974cc1a1
vishnugunipati <vishnugunipati@gmail.com>
2019-11-15 11:22:43 +0530

    updated

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,27 @@
 import React, { Component }from 'react';
-import { View, Text, } from 'react-native';
+import { View, StyleSheet, Text, } from 'react-native';
 
 export default class Header extends Component {
   render(){
     return(
       <View style={styles.container}>
-        <Text>hello header welcome to solve the world of testst</Text>
+        <Text style={styles.welcome}>hello header welcome to solve the world of testst</Text>
       </View>
     )
   }
 }
 
 const styles = {
-  container:{
-    background: '#f8f8f8  ',
-    paddingTop: 100,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#',
+  },
+
+  welcome: {
+    fontSize: 50,
+    textAlign: 'center',
+    margin: 5,
   },
 };
